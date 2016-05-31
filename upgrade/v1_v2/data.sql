917a28ce242a57af8df7fd77cbf5b9e331c6be25
Mike Swanson <mikeonthecomputer@gmail.com>
2016-05-30 23:16:44 -0700

    Remove upgrade scripts
    These are a pain to maintain and I doubt anyone else uses this.

--- upgrade/v1_v2/data.sql
+++
@@ -1,3 +0,0 @@
-COPY episode (id, series, title, airdate, remastered_airdate, season, episode_number, production_code, stardate, date, vignette) FROM stdin;
-ae4df5d2-5456-48f4-ad7e-8ab7bd54875b	51994c36-6748-4297-a179-efc44599cd21	Divided We Stand	2015-09-26	\N	\N	5	\N	6202.1	\N	f
-\.
